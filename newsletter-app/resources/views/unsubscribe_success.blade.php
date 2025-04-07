6803fcfec026bcba50031f672ea40a32b5083440
rodrigo-wong <wongmacr@gmail.com>
2025-04-06 20:50:24 -0400

    add unsubscribe success page for newsletter unsubscription confirmation

---
+++ newsletter-app/resources/views/unsubscribe_success.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Unsubscribed</title>
+</head>
+<body>
+    <h1>You have been successfully unsubscribed from the newsletter</h1>
+</body>
+</html>
