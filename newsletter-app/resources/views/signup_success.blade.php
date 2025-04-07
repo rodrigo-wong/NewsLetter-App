a560cdd130a94f69a69158837ef0fae6f8f00ce6
rodrigo-wong <wongmacr@gmail.com>
2025-04-06 20:50:14 -0400

    add signup success page for newsletter subscription confirmation

---
+++ newsletter-app/resources/views/signup_success.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Sign-up Successful</title>
+</head>
+<body>
+    <h1>You have signed up for the cryptocurrency newsletter successfully!</h1>
+</body>
+</html>
