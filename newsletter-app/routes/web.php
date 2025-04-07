46bb38f11186194ea66c56a718f4cb8e9fe95b6f
rodrigo-wong <wongmacr@gmail.com>
2025-04-06 20:51:13 -0400

    add routes for newsletter signup and unsubscription

---
+++ newsletter-app/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsletterSubscriberController;
+
+Route::get('/signup', [NewsletterSubscriberController::class, 'showSignupForm']);
+Route::post('/signup', [NewsletterSubscriberController::class, 'processSignup']);
+
+Route::get('/unsubscribe/{id}', [NewsletterSubscriberController::class, 'unsubscribe'])->name('unsubscribe');
+
+Route::get('/reload-captcha', function () {
+    return response()->json(['captcha' => captcha_img()]);
+});
